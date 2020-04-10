12318facede6062edc05948e11007f43a8c13fc4
michalbiesek <michal.biesek@intel.com>
2020-04-10 12:53:56 +0200

    Move client buffers explicitly to DRAM

--- src/networking.c
+++ src/networking.c
@@ -108,8 +108,8 @@ client *createClient(connection *conn) {
     c->name = NULL;
     c->bufpos = 0;
     c->qb_pos = 0;
-    c->querybuf = sdsempty();
-    c->pending_querybuf = sdsempty();
+    c->querybuf = sdsdramempty();
+    c->pending_querybuf = sdsdramempty();
     c->querybuf_peak = 0;
     c->reqtype = 0;
     c->argc = 0;
