a9bdd7412359fb5f082d98a92d77da658113e457
jschmieg <jakub.schmiegel@intel.com>
2020-05-05 13:35:26 +0200

    Robj which is not embedded allocated from DRAM

--- src/object.c
+++ src/object.c
@@ -42,7 +42,7 @@
 /* ===================== Creation and parsing of objects ==================== */
 
 robj *createObject(int type, void *ptr) {
-    robj *o = zmalloc(sizeof(*o));
+    robj *o = zmalloc_dram(sizeof(*o));
     o->type = type;
     o->encoding = OBJ_ENCODING_RAW;
     o->ptr = ptr;
@@ -365,7 +365,11 @@ static void _decrRefCount(robj *o, int on_dram) {
         case OBJ_STREAM: freeStreamObject(o); break;
         default: serverPanic("Unknown object type"); break;
         }
-        on_dram == OBJ_MEMORY_GENERAL ? zfree(o) : zfree_dram(o);
+        if (on_dram == OBJ_MEMORY_GENERAL || o->encoding == OBJ_ENCODING_EMBSTR) {
+            zfree(o);
+        } else {
+            zfree_dram(o);
+        }
     } else {
         if (o->refcount <= 0) serverPanic("decrRefCount against refcount <= 0");
         if (o->refcount != OBJ_SHARED_REFCOUNT) o->refcount--;
